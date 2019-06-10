6af69e3db41b800c9ce3e5afed71e34bad0cd192
TANIGUCHI Takaki <takaki@asis.media-as.org>
2019-06-10 15:59:56 +0900

    Updates

---
+++ precache-manifest.ee1ae0672734f6dc47517ff449226249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ad69ddf26a90449613e23702d39a5e",
+    "url": "/warikanjs/index.html"
+  },
+  {
+    "revision": "72edf021a4677f0a679d",
+    "url": "/warikanjs/static/css/main.4cc71b1d.chunk.css"
+  },
+  {
+    "revision": "f4e6423d28c33dfaf42b",
+    "url": "/warikanjs/static/js/2.326e2084.chunk.js"
+  },
+  {
+    "revision": "72edf021a4677f0a679d",
+    "url": "/warikanjs/static/js/main.f949da59.chunk.js"
+  },
+  {
+    "revision": "54efcb1e97b85c25ca12",
+    "url": "/warikanjs/static/js/runtime~main.66e15e8a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/warikanjs/precache-manifest.8b42b91f3184f6b9349a841273088a9c.js"
+  "/warikanjs/precache-manifest.ee1ae0672734f6dc47517ff449226249.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/warikanjs/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/warikanjs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
