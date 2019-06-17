0b846d817d424051f3afd5b1241db294b6068fd9
TANIGUCHI Takaki <takaki@asis.media-as.org>
2019-06-17 21:06:56 +0900

    use indexArray

--- src/models/DataStore.ts
+++ src/models/DataStore.ts
@@ -1,6 +1,7 @@
-import { array, deleteAt, modifyAt, snoc } from "fp-ts/lib/Array";
+import { array, deleteAt, snoc } from "fp-ts/lib/Array";
 import { monoidSum } from "fp-ts/lib/Monoid";
 import { Lens } from "monocle-ts";
+import { indexArray } from "monocle-ts/lib/Index/Array";
 import * as E from "./EntryState";
 
 export interface IDataStore {
@@ -13,6 +14,8 @@ export const defaultDataStore: IDataStore = {
 
 const entries = Lens.fromProp<IDataStore>()("entries");
 
+const indexEntry = indexArray<E.IEntryState>();
+
 export const addEntry = (amount: number, num: number) =>
   entries.modify(a => snoc(a, { amount, num }));
 
@@ -20,10 +23,10 @@ export const delEntry = (index: number) =>
   entries.modify(a => deleteAt(index, a).getOrElse(a));
 
 export const modifyAmount = (index: number, diff: number) =>
-  entries.modify(a => modifyAt(a, index, E.modifyAmount(diff)).getOrElse(a));
+  entries.modify(indexEntry.index(index).modify(E.modifyAmount(diff)));
 
 export const modifyNumber = (index: number, diff: number) =>
-  entries.modify(a => modifyAt(a, index, E.modifyNumber(diff)).getOrElse(a));
+  entries.modify(indexEntry.index(index).modify(E.modifyNumber(diff)));
 
 export const total = (self: IDataStore): number =>
   array.foldMap(monoidSum)(entries.get(self), E.entryTotal);
--- src/models/model.test.ts
+++ src/models/model.test.ts
@@ -1,63 +1,69 @@
-import { pipe } from 'fp-ts/lib/function';
+import { pipe } from "fp-ts/lib/function";
 import {
   addEntry,
   defaultDataStore,
   delEntry,
   modifyAmount,
   modifyNumber,
-  total,
-} from './DataStore';
-import * as E from './EntryState';
+  total
+} from "./DataStore";
+import * as E from "./EntryState";
 
 const ds = defaultDataStore;
 
-test('init DataStore', () => {
-  expect((ds.entries.length)).toBe(0);
+test("init DataStore", () => {
+  expect(ds.entries.length).toBe(0);
 });
 
-test('addEntry', () => {
+test("addEntry", () => {
   const added = addEntry(1000, 1)(ds);
   expect(added.entries.length).toBe(1);
   expect(added.entries[0].amount).toBe(1000);
   expect(added.entries[0].num).toBe(1);
 });
 
-test('delEntry', () => {
-  const added = pipe(addEntry(1000, 1),
-                     addEntry(2000, 2),
-                     addEntry(3000, 3),
-                     delEntry(1))(ds);
+test("delEntry", () => {
+  const added = pipe(
+    addEntry(1000, 1),
+    addEntry(2000, 2),
+    addEntry(3000, 3),
+    delEntry(1)
+  )(ds);
   expect(added.entries.length).toBe(2);
   expect(added.entries[1]!.amount).toBe(3000);
   expect(added.entries[1]!.num).toBe(3);
 });
 
-test('modify', () => {
-  const added = pipe(addEntry(1000, 1),
-                     addEntry(2000, 2),
-                     modifyAmount(0, 100),
-                     modifyNumber(1, 10))(ds);
+test("modify", () => {
+  const added = pipe(
+    addEntry(1000, 1),
+    addEntry(2000, 2),
+    modifyAmount(0, 100),
+    modifyNumber(1, 10)
+  )(ds);
   expect(added.entries[0]!.amount).toBe(1100);
   expect(added.entries[0]!.num).toBe(1);
   expect(added.entries[1]!.amount).toBe(2000);
   expect(added.entries[1]!.num).toBe(12);
 });
 
-test('total', () => {
-  const added = pipe(addEntry(1000, 1),
-                     addEntry(2000, 2),
-                     addEntry(3000, 3))(ds);
+test("total", () => {
+  const added = pipe(
+    addEntry(1000, 1),
+    addEntry(2000, 2),
+    addEntry(3000, 3)
+  )(ds);
   expect(total(added)).toBe(14000);
 });
 
-const es: E.IEntryState = ({ amount: 1000, num: 10 });
+const es: E.IEntryState = { amount: 1000, num: 10 };
 
-test('new EntryState', () => {
+test("new EntryState", () => {
   expect(es.amount).toBe(1000);
   expect(es.num).toBe(10);
 });
 
-test('diff', () => {
+test("diff", () => {
   expect(E.modifyAmount(100)(es).amount).toBe(1100);
   expect(E.modifyAmount(-100)(es).amount).toBe(900);
   expect(E.modifyAmount(-1100)(es).amount).toBe(0);
@@ -65,5 +71,4 @@ test('diff', () => {
   expect(E.modifyNumber(10)(es).num).toBe(20);
   expect(E.modifyNumber(-1)(es).num).toBe(9);
   expect(E.modifyNumber(-11)(es).num).toBe(0);
-
 });
