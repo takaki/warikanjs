93bc845e4bd321c12258b7fe3bec30fbfb2b3f2b
TANIGUCHI Takaki <takaki@asis.media-as.org>
2019-07-02 00:19:03 +0900

    split table Content

--- src/components/Warikan.tsx
+++ src/components/Warikan.tsx
@@ -7,8 +7,9 @@ import {
   TableRow
 } from "@material-ui/core";
 import { Add } from "@material-ui/icons";
+import { mapWithIndex } from "fp-ts/lib/Array";
 import * as React from "react";
-import { total, totalNumber } from "../models/DataStore";
+import { IDataStore, total, totalNumber } from "../models/DataStore";
 import * as E from "../models/EntryState";
 import { IWarikanProps } from "../types";
 import { DelButton } from "./DelButton";
@@ -16,89 +17,6 @@ import { ModifyAmount } from "./ModifyAmount";
 import { ModifyNumber } from "./ModifyNumber";
 
 export const Warikan = (props: IWarikanProps) => {
-  const tableBody = props.dataStore.entries.map(
-    (e: E.IEntryState, i: number) => {
-      return (
-        <React.Fragment key={i}>
-          <TableRow>
-            <TableCell className="or-amount-column">
-              <ModifyAmount
-                index={i}
-                diff={-500}
-                modifyAmount={props.updateAmount}
-              />
-            </TableCell>
-            <TableCell className="or-amount-column">
-              <ModifyAmount
-                index={i}
-                diff={-100}
-                modifyAmount={props.updateAmount}
-              />
-            </TableCell>
-            <TableCell className="or-amount-column" rowSpan={2}>
-              <Paper>
-                <div className="amount-line">{e.amount}円</div>
-                <div className="number-line">{e.num}人</div>
-              </Paper>
-            </TableCell>
-            <TableCell className="or-amount-column">
-              <ModifyAmount
-                index={i}
-                diff={100}
-                modifyAmount={props.updateAmount}
-              />
-            </TableCell>
-            <TableCell className="or-amount-column">
-              <ModifyAmount
-                index={i}
-                diff={500}
-                modifyAmount={props.updateAmount}
-              />
-            </TableCell>
-            <TableCell className="or-subtotal-column">
-              <DelButton index={i} onClick={props.delEntry} />
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="or-number-column">
-              <ModifyNumber
-                index={i}
-                diff={-5}
-                modifyNumber={props.updateNumber}
-              />
-            </TableCell>
-            <TableCell className="or-number-column">
-              <ModifyNumber
-                index={i}
-                diff={-1}
-                modifyNumber={props.updateNumber}
-              />
-            </TableCell>
-            <TableCell className="or-number-column">
-              <ModifyNumber
-                index={i}
-                diff={1}
-                modifyNumber={props.updateNumber}
-              />
-            </TableCell>
-            <TableCell className="or-number-column">
-              <ModifyNumber
-                index={i}
-                diff={5}
-                modifyNumber={props.updateNumber}
-              />
-            </TableCell>
-            <TableCell className="or-subtotal-column">
-              <div className="subtotal-box">
-                <span className="subtotal-line">{E.entryTotal(e)} 円</span>
-              </div>
-            </TableCell>
-          </TableRow>
-        </React.Fragment>
-      );
-    }
-  );
-
   return (
     <div className="App">
       <div className="total-line">
@@ -106,7 +24,14 @@ export const Warikan = (props: IWarikanProps) => {
         <span id="total-number">{totalNumber(props.dataStore)}</span> 人)
       </div>
       <Table>
-        <TableBody>{tableBody}</TableBody>
+        <TableBody>
+          <TableContent
+            dataStore={props.dataStore}
+            delEntry={props.delEntry}
+            updateAmount={props.updateAmount}
+            updateNumber={props.updateNumber}
+          />
+        </TableBody>
       </Table>
       <div>
         <Fab
@@ -122,3 +47,91 @@ export const Warikan = (props: IWarikanProps) => {
     </div>
   );
 };
+
+const TableContent: React.FC<{
+  dataStore: IDataStore;
+  updateAmount: (i: number, d: number) => void;
+  updateNumber: (i: number, d: number) => void;
+  delEntry: (i: number) => void;
+}> = props => (
+  <React.Fragment>
+    {mapWithIndex((i: number, e: E.IEntryState) => (
+      <React.Fragment key={i}>
+        <TableRow>
+          <TableCell className="or-amount-column">
+            <ModifyAmount
+              index={i}
+              diff={-500}
+              modifyAmount={props.updateAmount}
+            />
+          </TableCell>
+          <TableCell className="or-amount-column">
+            <ModifyAmount
+              index={i}
+              diff={-100}
+              modifyAmount={props.updateAmount}
+            />
+          </TableCell>
+          <TableCell className="or-amount-column" rowSpan={2}>
+            <Paper>
+              <div className="amount-line">{e.amount}円</div>
+              <div className="number-line">{e.num}人</div>
+            </Paper>
+          </TableCell>
+          <TableCell className="or-amount-column">
+            <ModifyAmount
+              index={i}
+              diff={100}
+              modifyAmount={props.updateAmount}
+            />
+          </TableCell>
+          <TableCell className="or-amount-column">
+            <ModifyAmount
+              index={i}
+              diff={500}
+              modifyAmount={props.updateAmount}
+            />
+          </TableCell>
+          <TableCell className="or-subtotal-column">
+            <DelButton index={i} onClick={props.delEntry} />
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="or-number-column">
+            <ModifyNumber
+              index={i}
+              diff={-5}
+              modifyNumber={props.updateNumber}
+            />
+          </TableCell>
+          <TableCell className="or-number-column">
+            <ModifyNumber
+              index={i}
+              diff={-1}
+              modifyNumber={props.updateNumber}
+            />
+          </TableCell>
+          <TableCell className="or-number-column">
+            <ModifyNumber
+              index={i}
+              diff={1}
+              modifyNumber={props.updateNumber}
+            />
+          </TableCell>
+          <TableCell className="or-number-column">
+            <ModifyNumber
+              index={i}
+              diff={5}
+              modifyNumber={props.updateNumber}
+            />
+          </TableCell>
+          <TableCell className="or-subtotal-column">
+            <div className="subtotal-box">
+              <span className="subtotal-line">{E.entryTotal(e)} 円</span>
+            </div>
+          </TableCell>
+        </TableRow>
+      </React.Fragment>
+    ))(props.dataStore.entries)}
+  </React.Fragment>
+);
--- src/models/DataStore.ts
+++ src/models/DataStore.ts
@@ -1,5 +1,7 @@
 import { array, deleteAt, snoc } from "fp-ts/lib/Array";
+import { constant } from "fp-ts/lib/function";
 import { monoidSum } from "fp-ts/lib/Monoid";
+import { getOrElse } from "fp-ts/lib/Option";
 import { Lens } from "monocle-ts";
 import { indexArray } from "monocle-ts/lib/Index/Array";
 import * as E from "./EntryState";
@@ -20,7 +22,7 @@ export const addEntry = (amount: number, num: number) =>
   entries.modify(a => snoc(a, { amount, num }));
 
 export const delEntry = (index: number) =>
-  entries.modify(a => deleteAt(index, a).getOrElse(a));
+  entries.modify(a => getOrElse(constant(a))(deleteAt(index)(a)));
 
 export const modifyAmount = (index: number, diff: number) =>
   entries.modify(indexEntry.index(index).modify(E.modifyAmount(diff)));
